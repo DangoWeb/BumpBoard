c4bf02a419dc4bc12ecca298bbb69458c251691b
Faisal N <contact@faisaln.com>
2026-09-23 00:52:18 -0400

    Pin new count message if multiple of 100

--- index.js
+++ index.js
@@ -222,6 +222,16 @@ client.on('messageCreate', async msg => {
     const bumpCount = msg.mentions.users.map(mentioned => (db.leaderboard[mentioned.id] && db.leaderboard[mentioned.id].count) || 0).reduce((a, b) => a + b, 0);
     const replyText = `🔼 ${msg.mentions.users.map(u => `<@${u.id}>`).join(', ')}'s total bump are now: **${bumpCount}**`;
     await msg.reply(replyText);
+    if (bumpCount % 100 === 0) {
+        const sentMsg = await msg.channel.messages.fetch({ limit: 1 }).then(messages => messages.first());
+        if (sentMsg) {
+            try {
+                await sentMsg.pin();
+            } catch (e) {
+                console.warn('Failed to pin message:', e);
+            };
+        };
+    };
 });
 
 client.on('guildMemberAdd', member => {
